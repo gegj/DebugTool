23d9248c34bf727264279e53b2fd9c408cf2f033
gegj <ulifuqiang@gmail.com>
2026-06-22 15:58:19 +0800

    移除 https://gh-proxy.org/，并更新版本到 1.1.9

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -21,9 +21,9 @@ using Microsoft.Win32;
 [assembly: AssemblyCompany("金恩出品")]
 [assembly: AssemblyProduct("DebugTool")]
 [assembly: AssemblyCopyright("Copyright © 金恩出品")]
-[assembly: AssemblyVersion("1.1.8.0")]
-[assembly: AssemblyFileVersion("1.1.8.0")]
-[assembly: AssemblyInformationalVersion("1.1.8")]
+[assembly: AssemblyVersion("1.1.9.0")]
+[assembly: AssemblyFileVersion("1.1.9.0")]
+[assembly: AssemblyInformationalVersion("1.1.9")]
 
 namespace DebugTool
 {
@@ -42,8 +42,8 @@ namespace DebugTool
     {
         private const string AppId = "my.zte.tool.v1";
         private const string AppTitle = "开启Debug调试工具 - 金恩出品";
-        private const string AppVersion = "1.1.8";
-        private const string UpdateJsonUrl = "https://gh-proxy.org/https://github.com/gegj/DebugTool/releases/latest/download/latest.json";
+        private const string AppVersion = "1.1.9";
+        private const string UpdateJsonUrl = "https://github.com/gegj/DebugTool/releases/latest/download/latest.json";
         private const string DefaultHost = "192.168.0.1";
         private const string DefaultRemoHost = "192.168.100.1";
         private const int WmSettingChange = 0x001A;
