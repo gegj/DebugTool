f1ca61066f24643bcc487c1e3b320d71de83ea08
gegj <ulifuqiang@gmail.com>
2026-06-22 13:48:50 +0800

    自动发布

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -43,7 +43,7 @@ namespace DebugTool
         private const string AppId = "my.zte.tool.v1";
         private const string AppTitle = "开启Debug调试工具 - 金恩出品";
         private const string AppVersion = "1.0.7";
-        private const string UpdateJsonUrl = "https://raw.githubusercontent.com/gegj/DebugTool/refs/heads/main/latest.json";
+        private const string UpdateJsonUrl = "https://github.com/gegj/DebugTool/releases/latest/download/latest.json";
         private const string DefaultHost = "192.168.0.1";
         private const string DefaultRemoHost = "192.168.100.1";
         private const int WmSettingChange = 0x001A;
